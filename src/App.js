07b1cd8b615622bbfb53ca44b22c750681f421bc
Folka2134 <folka2134@gmail.com>
2022-03-29 10:48:48 +0100

    Basetemplate + tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div className="App">
-      Template
+    <div className="h-screen bg-purple-400 grid grid-cols-1 lg:grid-cols-7 grid-rows-2  lg:grid-rows-none">
+      <div className="h-28 lg:h-auto bg-yellow-500 col-span-1">1</div>
+      <div className="col-span-5">2</div>
+      <div className="h-28 lg:h-auto bg-yellow-500 col-span-1">3</div>
     </div>
   );
 }
