9875d0b9a1e60026658f7ca3ceca35bc33f9d035
Folka2134 <folka2134@gmail.com>
2022-03-29 10:55:49 +0100

    Created base components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-function App() {
+import { Home } from "./components/Home.component";
+
+
+
+const App = () => {
   return (
     <div className="h-screen bg-purple-400 grid grid-cols-1 lg:grid-cols-7 grid-rows-2  lg:grid-rows-none">
       <div className="h-28 lg:h-auto bg-yellow-500 col-span-1">1</div>
-      <div className="col-span-5">2</div>
+      <div className="col-span-5"><Home /></div>
       <div className="h-28 lg:h-auto bg-yellow-500 col-span-1">3</div>
     </div>
   );
---
+++ src/components/Home.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Cover } from './Cover.component'
+import { Nav } from './Nav.component'
+
+export const Home = () => {
+  return (
+    <div>
+      <Nav />
+      <Cover />
+    </div>
+  )
+}
