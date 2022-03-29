3da7f49c06351fac2f9082c200b4b4745aaff538
Folka2134 <folka2134@gmail.com>
2022-03-29 11:42:42 +0100

    Updated grid

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import { Home } from "./components/Home.component";
 
 const App = () => {
   return (
-    <div className="h-screen bg-purple-400 grid grid-cols-1 lg:grid-cols-7 grid-rows-2  lg:grid-rows-none">
-      <div className="h-28 lg:h-auto bg-yellow-500 col-span-1">1</div>
-      <div className="col-span-5"><Home /></div>
-      <div className="h-28 lg:h-auto bg-yellow-500 col-span-1">3</div>
+    <div className="h-screen bg-purple-400 grid grid-cols-9">
+      <div className="h-auto bg-yellow-500 ">1</div>
+      <div className="col-span-7"><Home /></div>
+      <div className="h-auto bg-yellow-500 ">3</div>
     </div>
   );
 }
