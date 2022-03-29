a8da35dcff7d3288c2f554821513dcb006547426
Folka2134 <folka2134@gmail.com>
2022-03-29 12:37:48 +0100

    Updated navbar

--- src/components/Nav.component.jsx
+++ src/components/Nav.component.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='h-26 bg-black text-white text-8xl cursor-pointer '>
+      <ul className='h-full flex text-center font-tabloid transition-all'>
+        <li className='rounded-xs hover:bg-gray-600 hover:bg-opacity-30 hover:text-purple-500 px-5 duration-300 '>Catalog</li>
+        <li className='rounded-xs hover:bg-gray-600 hover:bg-opacity-30 hover:text-purple-500 px-5 duration-300'>News</li>
+      </ul>
+    </div>
   )
 }
