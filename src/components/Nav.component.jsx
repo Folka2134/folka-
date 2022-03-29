b7ec84a977206e3fc3c680475214e53809496b76
Folka2134 <folka2134@gmail.com>
2022-03-29 16:22:51 +0100

    Updated styles

--- src/components/Nav.component.jsx
+++ src/components/Nav.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Nav = () => {
   return (
-    <div className='h-26 bg-black text-white text-8xl cursor-pointer absolute top-0'>
+    <div className='h-26 w-[700px] bg-black text-white text-8xl cursor-pointer absolute top-0 flex justify-center'>
       <ul className='h-full flex text-center font-tabloid transition-all'>
         <button className='rounded-xs hover:text-purple-500 px-5 duration-300 '>Catalog</button>
         <button className='rounded-xs hover:text-purple-500 px-5 duration-300'>News</button>
