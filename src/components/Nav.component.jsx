0e8c42d37217576f95b5c2a68abf1d310d7e0688
Folka2134 <folka2134@gmail.com>
2022-03-30 13:29:20 +0100

    Updated Catalog font color

--- src/components/Nav.component.jsx
+++ src/components/Nav.component.jsx
@@ -4,7 +4,7 @@ export const Nav = () => {
   return (
     <div className='h-26 w-[400px] bg-black text-white text-8xl cursor-pointer absolute top-0 flex justify-center z-10'>
       <ul className='h-full flex text-center font-tabloid transition-all'>
-        <button className='rounded-xs hover:text-purple-500 px-5 duration-300 '>Catalog</button>
+        <button className='rounded-xs hover:text-[#8454DE] px-5 duration-300 '>Catalog</button>
         {/* <button className='rounded-xs hover:text-purple-500 px-5 duration-300'>News</button> */}
       </ul>
     </div>
