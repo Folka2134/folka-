82795931dfb452bb7f01529bc0dd68c6730d2ab2
Folka2134 <folka2134@gmail.com>
2022-03-30 20:09:03 +0100

    Updated cart styles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core'
 
+import { ImBin } from 'react-icons/im'
+import { CgEnter } from 'react-icons/cg'
+
 export const Cart = ({ cart }) => {
 
   const isEmpty = !cart.total_items
@@ -11,30 +14,32 @@ export const Cart = ({ cart }) => {
 
   const FilledCart = () => (
     <>
-      <Grid container spacing={3} className="flex-col justify-center">
+      <Grid container spacing={2} className="flex-col justify-center pt-6">
         {cart.line_items.map((item) => (
-          <Grid item xs={12} sm={4} key={item.id}>
+          <Grid item key={item.id}>
             {/* <CartItem /> */}
             <div>{item.name}</div>
           </Grid>
         ))}
       </Grid>
       <div className='flex-col mt-[10%] w-full justify-center'>
-        <Typography variant="h4">
+        <Typography className="text-right">
           Subtotal {cart.subtotal.formatted_with_symbol}
         </Typography>
-        <div className='content-center m-2'>
-          <Button className='min-w-[150px]' size="large" type="button" variant="contained">Empty Cart</Button>
-          <Button className='min-w-[150px]' size="large" type="button" variant="contained">Checkout</Button>
+        <div className='flex justify-evenly m-2'>
+          <Button className='min-w-[150px]' size="large" type="button" variant="contained"><ImBin /></Button>
+          <Button className='min-w-[150px]' size="large" type="button" variant="contained"><CgEnter /></Button>
         </div>
       </div>
     </>
   )
 
+  if (!cart.line_items) return 'Loading...'
+
   return (
-    <Container>
+    <Container className='font-tabloid'>
       <div className='h-full' />
-      <Typography className='mt-[5%]' variant='h3'>Your Shopping Cart</Typography>
+      <Typography className='pt-2 text-center'>Your Shopping Cart</Typography>
       {isEmpty ? <EmptyCart /> : <FilledCart />}
     </Container>
   )
