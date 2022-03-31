6a39440054820ea55ff7f1a54f874d940e277ff3
Folka2134 <folka2134@gmail.com>
2022-03-31 18:03:52 +0100

    Updated navbar

--- src/components/Nav.component.jsx
+++ src/components/Nav.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Nav = () => {
   return (
-    <div className='h-26 w-[400px] bg-black text-white text-8xl cursor-pointer absolute top-5 flex justify-center z-10'>
+    <div className='h-26 w-[400px] bg-black text-white text-8xl cursor-pointer absolute top-14 lg:top-5 flex justify-center z-10'>
       <ul className='h-full flex text-center font-tabloid transition-all'>
         <button className='rounded-xs hover:text-[#8454DE] px-5 duration-300 '>Catalog</button>
         {/* <button className='rounded-xs hover:text-purple-500 px-5 duration-300'>News</button> */}
