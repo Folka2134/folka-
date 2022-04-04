438c2f627c3ee2c30f56cf025ea34eb21c36c601
Folka2134 <folka2134@gmail.com>
2022-04-04 20:37:14 +0100

    Compressed main background and carousel images

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,15 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        "main": "url('images/1633867537922.jpg')",
+        "main": "url('images/1633867537922-min.jpg')",
         "sidebar1": "url('images/-folka-.png')",
         "logo2": "url('images/folkaLogo2.png')",
         "logo2yellow": "url('images/folkaLogo2yellow.png')",
         "logo2invert": "url('images/folkaLogo2invert.png')",
         "mainBanner": "url('images/-folka-rotated-redsmall.png')",
-        "banner1": "url('images/banner1.jpg')",
-        "banner2": "url('images/banner2.jpg')",
-        "banner3": "url('images/banner3.jpg')",
+        "banner1": "url('images/banner1-min.jpg')",
+        "banner2": "url('images/banner2-min.jpg')",
+        "banner3": "url('images/banner3-min.jpg')",
       },
       fontFamily: {
         tabloid: ["TABLOID"]
