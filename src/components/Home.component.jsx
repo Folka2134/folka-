33f689569a159445068a2bad68835558541dc38c
Folka2134 <folka2134@gmail.com>
2022-04-05 17:06:23 +0100

    Updated styles

--- src/components/Home.component.jsx
+++ src/components/Home.component.jsx
@@ -7,7 +7,7 @@ import { Products } from './Product/Products.component'
 
 export const Home = ({ products, handleAddToCart }) => {
   return (
-    <div className='h-screen w-full px-12'>
+    <div className='h-screen w-full lg:px-12'>
       <Nav />
       <Carousell />
       <Products products={products} handleAddToCart={handleAddToCart} />
