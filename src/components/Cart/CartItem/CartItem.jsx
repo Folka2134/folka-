766302d9793016a39d58447808ca66fa72e9e121
Folka2134 <folka2134@gmail.com>
2022-04-05 17:23:25 +0100

    Updated Cart Item styles

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core'
 
+import { ImBin } from 'react-icons/im'
+
 export const CartItem = ({ item }) => {
   return (
-    <Card>
-      <CardMedia image={item.image.url} alt={item.name} className="{media}" />
-      <CardContent className='{cardContent}'>
+    <Card className='w-[200px]'>
+      <CardMedia image={item.image.url} alt={item.name} className="h-[100px] w-[200px]" />
+      <CardContent className='flex justify-between'>
         <Typography>{item.name}</Typography>
         <Typography>{item.line_total.formatted_with_symbol}</Typography>
       </CardContent>
-      <CardActions className="{cardActions}">
-        <div className='{buttons}'>
+      <CardActions className="flex justify-between">
+        <div className='flex items-center'>
           <Button type="button" size="small">-</Button>
           <Typography>{item.quantity}</Typography>
           <Button type="button" size="small">+</Button>
         </div>
-        <Button variant="contained" type="button">Remove</Button>
+        <Button type="button"><ImBin /></Button>
       </CardActions>
     </Card>
   )
