c55eca89d91f4f25c800b7a5a96c19b5ea8060d1
Folka2134 <folka2134@gmail.com>
2022-04-07 17:18:08 +0100

    Updated nav font size

--- src/components/Nav.component.jsx
+++ src/components/Nav.component.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const Nav = () => {
   return (
-    <div className='h-26 w-[400px] bg-black text-white text-8xl cursor-pointer absolute top-14 lg:top-5 flex justify-center z-20'>
+    <div className='h-26 w-[250px] bg-black text-white text-6xl cursor-pointer absolute top-20 lg:top-5 flex justify-center z-20'>
       <ul className='h-full flex text-center font-tabloid transition-all'>
-        <button className='rounded-xs hover:text-[#8454DE] px-5 duration-300 '>Catalog</button>
+        <button className='rounded-xs hover:text-[#8353DC] px-5 duration-300 '>Catalog</button>
         {/* <button className='rounded-xs hover:text-purple-500 px-5 duration-300'>News</button> */}
       </ul>
     </div>
