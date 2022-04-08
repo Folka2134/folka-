81ca609c43926dd7417c0cef99c41ece3dcaaf25
Folka2134 <folka2134@gmail.com>
2022-04-08 12:02:00 +0100

    Updated mobile width

--- src/components/Home.component.jsx
+++ src/components/Home.component.jsx
@@ -7,7 +7,7 @@ import { Products } from './Product/Products.component'
 
 export const Home = ({ products, handleAddToCart }) => {
   return (
-    <div className='h-screen w-full lg:px-12'>
+    <div className='h-screen min-w-[250px] lg:w-full lg:px-12'>
       <Nav />
       <Carousell />
       <Products products={products} handleAddToCart={handleAddToCart} />
